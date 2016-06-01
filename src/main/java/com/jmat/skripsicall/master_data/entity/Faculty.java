462b3e5774c0e0c8cd236a3244501ac78b557908
Hasani <abdul.kadir.hasani@basajans.com>
2016-06-01 14:16:29 +0700

    entity fakultas digunakan untuk menampung data fakultas

---
+++ src/main/java/com/jmat/skripsicall/master_data/entity/Faculty.java
@@ -0,0 +1,37 @@
+package com.jmat.skripsicall.master_data.entity;
+
+import com.jmat.skripsicall.support.entity.AbstractEntity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by Hasani on 6/1/2016.
+ */
+@Entity
+@Table(name = "mtd_faculty")
+public class Faculty extends AbstractEntity{
+
+    @Column(name = "nm_faculty",length = 65)
+    private String nameFaculty;
+    @Column(name = "count_prodi",length = 3)
+    private Integer countProdi;
+
+    public String getNameFaculty() {
+        return nameFaculty;
+    }
+
+    public void setNameFaculty(String nameFaculty) {
+        this.nameFaculty = nameFaculty;
+    }
+
+    public Integer getCountProdi() {
+        return countProdi;
+    }
+
+    public void setCountProdi(Integer countProdi) {
+        this.countProdi = countProdi;
+    }
+}
