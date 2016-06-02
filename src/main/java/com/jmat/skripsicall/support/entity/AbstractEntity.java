9961d0412f67284033e480952c9d2beed6eb1ed4
Hasani <abdul.kadir.hasani@basajans.com>
2016-06-02 16:02:37 +0700

    menambahkan huruf d pada createAt menjadi createdAt jg pada updateAt menjadi updatedAt

--- src/main/java/com/jmat/skripsicall/support/entity/AbstractEntity.java
+++ src/main/java/com/jmat/skripsicall/support/entity/AbstractEntity.java
@@ -22,16 +22,31 @@ public abstract class AbstractEntity implements IEntity {
 
     @Column(name = "create_at", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private Date createAt;
+    private Date createdAt;
+
+    @Column(name = "created_by_id", nullable = false)
+    @Type(type="pg-uuid")
+    @JsonProperty("created_by_id")
+    private UUID createdById;
 
     @Column(name = "update_at", nullable = false)
     @Temporal(TemporalType.TIMESTAMP)
-    private Date updateAt;
+    private Date updatedAt;
+
+    @Column(name = "updated_by_id", nullable = false)
+    @Type(type="pg-uuid")
+    @JsonProperty("updated_by_id")
+    private UUID updatedById;
 
     @Column(name = "deleted_at")
     @Temporal(TemporalType.TIMESTAMP)
     @JsonProperty("deleted_at")
     private Date deletedAt;
+
+    @Column(name = "deleted_by_id")
+    @Type(type="pg-uuid")
+    @JsonProperty("deleted_by_id")
+    private UUID deletedById;
 
     public UUID getId() {
         return id;
@@ -41,20 +56,36 @@ public abstract class AbstractEntity implements IEntity {
         this.id = id;
     }
 
-    public Date getCreateAt() {
-        return createAt;
+    public Date getCreatedAt() {
+        return createdAt;
     }
 
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
+    public void setCreatedAt(Date createAt) {
+        this.createdAt = createAt;
     }
 
-    public Date getUpdateAt() {
-        return updateAt;
+    public UUID getCreatedById() {
+        return createdById;
     }
 
-    public void setUpdateAt(Date updateAt) {
-        this.updateAt = updateAt;
+    public void setCreatedById(UUID createdById) {
+        this.createdById = createdById;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updateAt) {
+        this.updatedAt = updateAt;
+    }
+
+    public UUID getUpdatedById() {
+        return updatedById;
+    }
+
+    public void setUpdatedById(UUID updatedById) {
+        this.updatedById = updatedById;
     }
 
     public Date getDeletedAt() {
@@ -65,13 +96,21 @@ public abstract class AbstractEntity implements IEntity {
         this.deletedAt = deletedAt;
     }
 
+    public UUID getDeletedById() {
+        return deletedById;
+    }
+
+    public void setDeletedById(UUID deletedById) {
+        this.deletedById = deletedById;
+    }
+
     @PrePersist
     void prePersist() {
-        this.createAt = this.updateAt = new Date();
+        this.createdAt = this.updatedAt = new Date();
     }
 
     @PreUpdate
     void preUpdate() {
-        this.updateAt = new Date();
+        this.updatedAt = new Date();
     }
 }
